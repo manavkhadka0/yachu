692cf5001af0a9aca2b8905fc624a215e59fe772
prasannakarki77 <prasanna.karki77@gmail.com>
2023-11-21 19:47:54 +0545

    header added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Header from "@/components/header/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Button</Button>
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <Header />
     </main>
   );
 }
---
+++ components/header/Header.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./navbar/Navbar";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+export default Header;
