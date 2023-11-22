e973713f6d0bdc6c2e0794477eae042ff8e27d1a
prasannakarki77 <prasanna.karki77@gmail.com>
2023-11-22 19:58:26 +0545

    quick links below hero banner

---
+++ components/hero/HeroQuickLinks.tsx
@@ -0,0 +1,40 @@
+import { HelpCircleIcon, MessagesSquareIcon, TruckIcon } from "lucide-react";
+import Link from "next/link";
+
+const HERO_QUICK_LINKS = [
+  {
+    title: "Track Order",
+    Icon: TruckIcon,
+    href: "/",
+  },
+  {
+    title: "Chat With Us",
+    Icon: MessagesSquareIcon,
+    href: "/",
+  },
+  {
+    title: "FAQs",
+    Icon: HelpCircleIcon,
+    href: "/",
+  },
+];
+
+const HeroQuickLinks = () => {
+  return (
+    <div className="flex justify-between gap-4 mx-auto ">
+      {HERO_QUICK_LINKS.map(({ href, Icon, title }, index) => (
+        <Link
+          href={href}
+          className=" flex  sm:flex-row flex-col gap-2 items-center  hover:text-amber-700 transition-all"
+          key={index}
+        >
+          <Icon className="sm:h-10 sm:w-10 h-6 w-6 text-amber-700" />{" "}
+          <span className="sm:text-lg text-base text-center  font-semibold">
+            {title}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+export default HeroQuickLinks;
