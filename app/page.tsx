ebc62c11af6446b9e6fe491039014160bd196c5b
prasannakarki77 <prasanna.karki77@gmail.com>
2023-11-23 20:42:20 +0545

    layout and page refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
 import BlogSection from "@/components/blog/BlogSection";
 import FlowerDivider from "@/components/custom-ui/FlowerDivider";
-import Footer from "@/components/footer/Footer";
-import Header from "@/components/header/Header";
+
 import Hero from "@/components/hero/Hero";
 import ProductShowcase from "@/components/product/ProductShowcase";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col ">
-      <Header />
+    <main className="flex flex-col ">
       <Hero />
       <FlowerDivider />
       <ProductShowcase />
       <BlogSection />
-      <Footer />
     </main>
   );
 }
