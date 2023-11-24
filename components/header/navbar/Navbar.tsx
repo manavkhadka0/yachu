078d1361fcee9d195ccf3c40e07bbd92cfb3c829
prasannakarki77 <prasanna.karki77@gmail.com>
2023-11-24 18:21:17 +0545

    nav bar logo link

--- components/header/navbar/Navbar.tsx
+++ components/header/navbar/Navbar.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import { NavMenus } from "./NavMenus";
 import NavActions from "./NavActions";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 items-center  p-4 container mx-auto">
       <div className="">
-        <Image
-          src="/logo.jpg"
-          alt="me"
-          width="500"
-          height="500"
-          className="w-16"
-        />
+        <Link href={"/"}>
+          <Image
+            src="/logo.jpg"
+            alt="me"
+            width="500"
+            height="500"
+            className="w-16"
+          />
+        </Link>
       </div>
       <div className=" hidden md:block ">
         <NavMenus />
