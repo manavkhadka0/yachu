63ff83f95beeab468344bb4d8d859a3d484951aa
prasannakarki77 <prasanna.karki77@gmail.com>
2023-12-02 19:16:34 +0545

    hero section made responsive

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -5,47 +5,58 @@ import { HERO_BEFORE_AFTER } from "@/constants/product";
 
 const Hero = () => {
   return (
-    <div className="container pt-12    flex gap-2 ">
+    <div className="container pt-16 sm:pt-12 md:flex-row flex-col flex gap-2 justify-between md:items-start items-center ">
       <div className="">
-        <div className=" flex gap-6 items-center mb-6">
-          <div className="">
+        <div className=" flex gap-1 md:gap-6 items-center mb-6 md:justify-start justify-center">
+          <div className=" flex flex-col items-center gap-2">
             <Image
               src={"/before.jpeg"}
               alt="Dandruff"
               height={200}
               width={200}
-              className=" h-20  w-20 rounded-full object-cover  "
+              className=" h-12 w-12 md:h-16 md:w-16 lg:h-20  lg:w-20 rounded-full object-cover  "
             />
-            <p className="  text-xl">Dandruff?</p>
+            <p className="  md:text-xl">Dandruff?</p>
+          </div>
+          <MoveRightIcon className=" mb-2 " />
+          <div className="flex flex-col items-center gap-2">
+            <Image
+              src={"/before.jpeg"}
+              alt="Dandruff"
+              height={200}
+              width={200}
+              className=" h-12 w-12 md:h-16 md:w-16 lg:h-20  lg:w-20 rounded-full object-cover  "
+            />
+            <p className=" md:text-xl font-semibold">Hairfall?</p>
           </div>
           <MoveRightIcon className=" mb-2" />
-          <div className="">
+          <div className=" flex flex-col items-center gap-2">
             <Image
               src={"/before.jpeg"}
               alt="Dandruff"
               height={200}
               width={200}
-              className=" h-20  w-20 rounded-full object-cover  "
+              className=" h-12 w-12 md:h-16 md:w-16 lg:h-20  lg:w-20 rounded-full object-cover  "
             />
-            <p className=" text-xl font-semibold">Hairfall?</p>
+            <p className=" md:text-xl font-extrabold">Baldness?</p>
           </div>
-          <MoveRightIcon className=" mb-2" />
-          <div className="">
-            <Image
-              src={"/before.jpeg"}
-              alt="Dandruff"
-              height={200}
-              width={200}
-              className=" h-20  w-20 rounded-full object-cover  "
-            />
-            <p className=" text-xl font-extrabold">Baldness?</p>
-          </div>
+        </div>
+        <div className=" md:hidden   w-full flex flex-col items-center mb-6">
+          <Image
+            src={"/yachu-hero.svg"}
+            alt="curve line"
+            height={500}
+            width={500}
+            className=" max-w-[200px] "
+          />
         </div>
         <div className="">
           <p className=" text-xl font-bold mb-2">
             Easy, Affordable and Safe ✅
           </p>
-          <h1 className=" text-6xl font-bold">Yachu Hair Oil</h1>
+          <h1 className=" text-4xl md:text-5xl lg:text-6xl font-bold">
+            Yachu Hair Oil
+          </h1>
           <div className=" space-y-2 my-6">
             <p>❌ Dandruff, Hair Loss, Baldness ? Multiple Hair Problems</p>
             <p>🟢 Ultimate Solution = Yachu Hair Oil</p>
@@ -53,7 +64,7 @@ const Hero = () => {
             <p>🇳🇵 Made in Nepal</p>
           </div>
         </div>
-        <div className=" flex mb-4">
+        <div className=" flex mb-4 flex-wrap gap-4">
           {HERO_BEFORE_AFTER.map(({ before, after }, index) => (
             <div className="flex " key={index}>
               <Image
@@ -61,19 +72,21 @@ const Hero = () => {
                 alt="Before Yachu"
                 height={200}
                 width={200}
-                className=" h-20  w-20 rounded-full object-cover opacity-70 "
+                className=" h-16 w-16 lg:h-20  lg:w-20 rounded-full object-cover opacity-70 "
               />
               <Image
                 src={after}
                 alt="After Yachu"
                 height={200}
                 width={200}
-                className=" h-20  w-20 rounded-full object-cover  -translate-x-6 "
+                className=" h-16 w-16 lg:h-20  lg:w-20 rounded-full object-cover  -translate-x-6 "
               />
             </div>
           ))}
           <div className=" flex flex-col gap-1">
-            <p className=" text-amber-700 text-6xl  font-extrabold">50K +</p>
+            <p className=" text-amber-700 text-4xl md:text-6xl  font-extrabold">
+              50K +
+            </p>
             <p>Hair Problems Solved</p>
           </div>
         </div>
@@ -82,44 +95,44 @@ const Hero = () => {
           variant={"ghost"}
           className=" flex gap-2 hover:gap-4 transition-all"
         >
-          <span>Explore More Yachu’s Hair Transformation</span>{" "}
+          <span className="">Explore More Transformation</span>{" "}
           <MoveRightIcon />
         </Button>
       </div>
       {/* <EmblaCarousel /> */}
       {/* <HeroQuickLinks /> */}
-      <div className=" relative flex-1">
+      <div className=" relative lg:flex-1">
         <Image
           src={"/curve-line.svg"}
           alt="curve line"
           height={300}
           width={700}
-          className="  translate-y-6 -translate-x-16 "
+          className=" w-[600px] xl:w-auto translate-y-6 -translate-x-16 lg:block hidden "
         />
-        <div className=" absolute top-5 left-28 ">
+        <div className=" xl:block hidden absolute top-7 left-28 ">
           <Image
             src={"/person.png"}
             alt="curve line"
             height={400}
             width={700}
-            className=" w-[240px]      "
+            className=" w-[220px]      "
           />
 
           <div className="py-4">
             <p className="text-amber-600 text-lg font-bold">
-              पुर्खा को फर्मुला अब नयाँ टेक्नोलोजी क साथ 1
+              पुर्खा को फर्मुला अब नयाँ टेक्नोलोजी क साथ
             </p>
             <p className="text-lg">Reverse the Hair Damage Process</p>
           </div>
-          <div className=" w-full">
-            <Image
-              src={"/yachu-hero.svg"}
-              alt="curve line"
-              height={1000}
-              width={100}
-              className=" w-[700px]  absolute  top-0 -right-[300px]  "
-            />
-          </div>
+        </div>
+        <div className=" w-full">
+          <Image
+            src={"/yachu-hero.svg"}
+            alt="curve line"
+            height={500}
+            width={500}
+            className=" w-[300px] xl:w-[370px]  lg:absolute  xl:top-[180px] top-[100px] right-[6px] md:block hidden  "
+          />
         </div>
       </div>
     </div>
