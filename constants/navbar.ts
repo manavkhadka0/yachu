8dc2c8dbf3617a1cb62a3444cb01c94a6d76bcfc
prasannakarki77 <prasanna.karki77@gmail.com>
2023-12-03 19:10:38 +0545

    build issue fixed

--- constants/navbar.ts
+++ constants/navbar.ts
@@ -1,6 +1,16 @@
 import { Routes } from "@/utils/routes";
 
-export const NAVBAR_LINKS = [
+type NavbarLinkProps = {
+  title: string;
+  options?: {
+    title: "Hair Care";
+    href: "";
+    description: "A modal dialog that interrupts the user with important content and expects a response.";
+  }[];
+  href: string;
+};
+
+export const NAVBAR_LINKS: NavbarLinkProps[] = [
   {
     title: "Home",
     href: Routes.home.root,
