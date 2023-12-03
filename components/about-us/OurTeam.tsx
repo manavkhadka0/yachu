a01ce3d1a5d1692cc688d1041418a98f6ab74185
prasannakarki77 <prasanna.karki77@gmail.com>
2023-12-03 20:48:23 +0545

    about and out team design fix

--- components/about-us/OurTeam.tsx
+++ components/about-us/OurTeam.tsx
@@ -3,24 +3,27 @@ import Image from "next/image";
 
 const OurTeam = () => {
   return (
-    <div className=" flex flex-col  items-center sm:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 gap-4  sm:gap-10 xl:gap-16">
-      {OUR_TEAM.map((member) => (
-        <div className=" max-w-xs mx:auto " key={member.name}>
-          <Image
-            src={member.imageSrc}
-            alt={member.position}
-            height={700}
-            width={700}
-            className=" rounded-xl h-96  object-cover  shadow-lg"
-          />
-          <div className=" flex flex-col gap-1 mt-4">
-            <p className=" sm:text-lg font-medium">{member.name}</p>
-            <p className=" text-sm  sm:text-base font-medium text-gray-400">
-              {member.position}
-            </p>
+    <div className=" container mb-12">
+      <h2 className=" text-3xl font-bold mb-6">Our Team</h2>
+      <div className=" flex flex-col  items-center sm:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-5 gap-4  sm:gap-8 ">
+        {OUR_TEAM.map((member) => (
+          <div className=" max-w-xs mx:auto " key={member.name}>
+            <Image
+              src={member.imageSrc}
+              alt={member.position}
+              height={700}
+              width={700}
+              className=" rounded-xl md:h-80 h-96  object-cover  shadow-lg"
+            />
+            <div className=" flex flex-col gap-1 mt-4">
+              <p className=" sm:text-lg font-medium">{member.name}</p>
+              <p className=" text-sm  sm:text-base font-medium text-gray-400">
+                {member.position}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>{" "}
     </div>
   );
 };
