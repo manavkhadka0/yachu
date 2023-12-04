3d0da135899ef4a9f6fede8922d691ea68f8db48
prasannakarki77 <prasanna.karki77@gmail.com>
2023-12-04 08:56:58 +0545

    video showcase changed

--- components/video/VideoShowcase.tsx
+++ components/video/VideoShowcase.tsx
@@ -17,35 +17,47 @@ const VIDEOS = [
 
 const VideoShowcase = () => {
   return (
-    <div className=" my-12 container ">
-      <Swiper
-        slidesPerView={4}
-        spaceBetween={5}
-        pagination={{
-          clickable: true,
-          el: ".swiper-pagination",
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-        style={{
-          paddingBottom: "40px",
-        }}
-      >
-        {VIDEOS.map((video, index) => (
-          <SwiperSlide key={index}>
-            <iframe
-              height="580"
-              src={video}
-              title="Yachu Hair Oil"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            ></iframe>
-          </SwiperSlide>
-        ))}
-        <div className="swiper-pagination" style={{}} />{" "}
-        {/* Customize pagination color here */}
-      </Swiper>
+    <div className=" flex gap-4 overflow-x-scroll  justify-center container mt-16">
+      {VIDEOS.map((video, index) => (
+        <iframe
+          height="580"
+          width={300}
+          src={video}
+          title="Yachu Hair Oil"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          key={index}
+        ></iframe>
+      ))}
     </div>
+    // <div className=" my-12 container ">
+    //   <Swiper
+    //     slidesPerView={3}
+    //     spaceBetween={5}
+    //     pagination={{
+    //       clickable: true,
+    //       el: ".swiper-pagination",
+    //     }}
+    //     navigation={true}
+    //     modules={[Pagination, Navigation]}
+    //     className="mySwiper"
+    //     style={{
+    //       paddingBottom: "40px",
+    //     }}
+    //   >
+    //     {VIDEOS.map((video, index) => (
+    //       <SwiperSlide key={index}>
+    //         <iframe
+    //           height="580"
+    //           src={video}
+    //           title="Yachu Hair Oil"
+    //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    //         ></iframe>
+    //       </SwiperSlide>
+    //     ))}
+    //     <div className="swiper-pagination" style={{}} />{" "}
+    //     {/* Customize pagination color here */}
+    //   </Swiper>
+    // </div>
   );
 };
 export default VideoShowcase;
