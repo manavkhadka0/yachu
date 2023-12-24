630041e94080048c5449e1f7dcb67ef4a2f29ce6
prasannakarki77 <prasanna.karki77@gmail.com>
2023-12-24 19:25:54 +0545

    contact form fixes

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -4,13 +4,23 @@ import FAQ from "./FAQ";
 
 const Contact = () => {
   return (
-    <div className="w-full pt-12 container pb-16" id="contact">
-      <div className=" grid sm:grid-flow-col grid-flow-row sm:grid-cols-2 mx-auto  gap-10 ">
-        <ContactForm />
+    <div className=" pt-12  pb-16 w-full" id="contact">
+      <div className=" grid sm:grid-flow-col grid-flow-row sm:grid-cols-2 w-full gap-10 ">
+        <div className=" p-5 sm:p-8 shadow-lg  rounded-lg ">
+          <div className=" mb-10 ">
+            <h1 className=" text-2xl sm:text-3xl font-bold mb-1">
+              Contact Yachu
+            </h1>
+            <p className=" text-slate-500 font-medium text-sm sm:text-base">
+              Fill in your details and send us a message
+            </p>
+          </div>
+          <ContactForm />
+        </div>
         <ContactDetails />
       </div>
-      <div className="mx-auto mt-16 ">
-        <h3 className="sm:text-2xl text-xl font-bold">FAQs</h3>
+      <div className=" w-full mt-16 ">
+        <h3 className=" sm:text-2xl text-xl font-bold">FAQs</h3>
         <FAQ />
       </div>
     </div>
--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -37,59 +37,51 @@ const ContactForm = () => {
     window.alert("message sent");
   };
   return (
-    <div className=" p-5 sm:p-8 shadow-lg rounded-lg ">
-      <div className=" mb-4 ">
-        <h1 className=" text-2xl sm:text-3xl font-bold mb-1"> Contact Yachu</h1>
-        <p className=" text-slate-500 font-medium text-sm sm:text-base">
-          Fill in your details and send us a message
-        </p>
-      </div>
-      <Form {...form}>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
-          <div className=" grid  sm:grid-cols-2 sm:gap-2 gap-5">
-            <RHFInput
-              name="name"
-              label="Full Name"
-              placeholder="eg. John Doe"
-              required
-              className="text-base"
-            />
-            <RHFInput
-              name="phone"
-              label="Phone Number"
-              placeholder="eg. 986543665"
-              type="number"
-              className="text-base"
-              required
-            />
-          </div>
+    <Form {...form}>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+        <div className=" grid  sm:grid-cols-2 sm:gap-4 gap-8">
           <RHFInput
-            name="email"
-            label="Email Address"
-            placeholder="eg. john@gmail.com"
-            type="email"
+            name="name"
+            label="Full Name"
+            placeholder="eg. John Doe"
             required
             className="text-base"
           />
-
-          <RHFTextarea
-            name="message"
-            label="Message"
-            rows={5}
-            placeholder="Write your message..."
-            className="text-base py-4 "
+          <RHFInput
+            name="phone"
+            label="Phone Number"
+            placeholder="eg. 986543665"
+            type="number"
+            className="text-base"
+            required
           />
-          <div className="flex justify-end">
-            <Button type="submit" disabled={isSubmitting}>
-              {isSubmitting && (
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-              )}
-              Submit
-            </Button>
-          </div>
-        </form>
-      </Form>
-    </div>
+        </div>
+        <RHFInput
+          name="email"
+          label="Email Address"
+          placeholder="eg. john@gmail.com"
+          type="email"
+          required
+          className="text-base"
+        />
+
+        <RHFTextarea
+          name="message"
+          label="Message"
+          rows={5}
+          placeholder="Write your message..."
+          className="text-base py-4 "
+        />
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="w-full text-lg py-6 font-bold"
+        >
+          {isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+          Submit
+        </Button>
+      </form>
+    </Form>
   );
 };
 
