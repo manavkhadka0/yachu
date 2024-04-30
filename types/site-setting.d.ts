eec2c3c0600399f27240e8c428433390ee6612db
Ram Rijal <87277221+RamRijal@users.noreply.github.com>
2024-04-30 15:15:34 +0545

    "Refactor: replace hardcoded site config with async fetch, update About component to use async fetch for site config" About.Our_story and About_founder updated

--- types/site-setting.d.ts
+++ types/site-setting.d.ts
@@ -1,26 +1,11 @@
-// [
-//     {
-//         "id": 1,
-//         "meta_title": "Meta Title Landing Page",
-//         "meta_description": "Meta Description Landing Page",
-//         "hero_title": "Title",
-//         "hero_section_subtitle": "Discover The Best Hiking Trails And Bee-Watching Spots On Your Next Adventure. Book A Trip Now",
-//         "hero_section_image": null,
-//         "about_founder": "<p>Hello</p>",
-//         "message_from_ceo": "<p>whats up</p>",
-//         "our_story": "<p>dsadasd</p>"
-//     }
-// ]
-
 export type TSiteSetting = {
   id: number;
+  our_story: string;
+  about_founder: string;
+  message_from_ceo: string;
   meta_title: string;
   meta_description: string;
   hero_title: string;
   hero_section_subtitle: string;
   hero_section_image: string;
-  about_founder: string;
-  message_from_ceo: string;
-  our_story: string;
 }[];
-
