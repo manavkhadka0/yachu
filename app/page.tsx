7e0311862877e2aa10855367428c443bdc760ced
Ram Rijal <87277221+RamRijal@users.noreply.github.com>
2024-05-01 18:13:15 +0545

    x

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ import BlogSection from "@/components/blog/BlogSection";
 import { BASE_API_URL } from "@/utils/config";
 import { TSiteSetting } from "@/types/site-setting";
 import Testimonials from "@/components/home/Testimonials";
-import Gallery from "@/components/gallery/Gallery";
+// import Gallery from "@/components/gallery/Gallery";
 
 
 
@@ -37,7 +37,7 @@ export default async function Home() {
       {/* ---------------------------------- */}
       <FlowerDivider />
       {/* --------- About Yachu ------------*/}
-      <About aboutdetails={[]}  />
+      <About aboutdetails={[]} />
       {/* ---------------------------------- */}
 
       <Questions />
